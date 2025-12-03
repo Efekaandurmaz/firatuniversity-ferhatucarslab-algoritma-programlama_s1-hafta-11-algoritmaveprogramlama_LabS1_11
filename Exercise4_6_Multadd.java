265a3721951c7a8116b96f1e48ac004480cc8d10
Efekaandurmaz <250541106@firat.edu.tr>
2025-12-03 21:52:27 +0300

    Implement multadd and expSum methods with tests

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,15 @@
 public class Exercise4_6_Multadd {
 
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a * b + c;
     }
 
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+        System.out.println(multadd(1.0, 2.0, 3.0));
+        System.out.println(expSum(2.0));
     }
 }
